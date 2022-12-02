dad03ca4aea665afb132a22b4e99ed115212a919
domenicatonziello <domenica.tonziello10@gmail.com>
2022-12-02 15:06:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('JS OK');
+
+/* Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/ 
+
+/*
+1. Prendere elemento dal DOM
+2. Creare variabile n.21
+3. Chiedere il nome all'utente
+4. Chiedere il cognome all'utente
+5. Chiedere il colore preferito all'utente
+6. Creare variabile con calcolo pwd
+7. Riportare nel DOM
+*/
+
